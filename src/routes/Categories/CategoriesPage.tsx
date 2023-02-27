5a2632fbc7254487d92b6b1e6d27e20d363efcea
beyerenrico <enrico.beyer@synaigy.com>
2023-02-27 22:37:14 +0100

    :lipstick: Fix UI styling

--- src/routes/Categories/CategoriesPage.tsx
+++ src/routes/Categories/CategoriesPage.tsx
@@ -71,6 +71,8 @@ function Categories({}: Props) {
             xs: "1fr",
             sm: "repeat(2, 1fr)",
             md: "repeat(3, 1fr)",
+            lg: "repeat(4, 1fr)",
+            xl: "repeat(5, 1fr)",
           },
         }}
       >
