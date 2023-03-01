0b122df2937a21e73f705ee5588a876da8563547
beyerenrico <enrico.beyer@synaigy.com>
2023-03-01 11:41:33 +0100

    :sparkles: Add custom styling

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,55 @@
 import { createTheme } from "@mui/material";
-import { blue, grey, orange } from "@mui/material/colors";
+import { blue, grey, indigo, orange } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: grey[900],
+      main: indigo[600],
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+          fontSize: "0.875rem",
+          fontWeight: 500,
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          boxShadow:
+            "0 0 #0000, 0 0 #0000, 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+          borderRadius: 8,
+        },
+      },
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          paddingTop: 0,
+          paddingBottom: 0,
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          paddingTop: 0,
+          paddingBottom: 0,
+        },
+      },
+    },
+    MuiAutocomplete: {
+      styleOverrides: {
+        root: {
+          "& .MuiAutocomplete-inputRoot": {
+            padding: 0,
+          },
+        },
+      },
     },
   },
 });
