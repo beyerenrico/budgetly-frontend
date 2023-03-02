3019c0111d50efff95a65fac7836af934e2dd65c
beyerenrico <enrico.beyer@synaigy.com>
2023-03-02 22:46:25 +0100

    :sparkles: Add new styling

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,13 @@ import { Notifications } from "@mantine/notifications";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <MantineProvider withNormalizeCSS withGlobalStyles>
+    <MantineProvider
+      withNormalizeCSS
+      withGlobalStyles
+      theme={{
+        colorScheme: "dark",
+      }}
+    >
       <Notifications />
       <RouterProvider router={router} />
     </MantineProvider>
